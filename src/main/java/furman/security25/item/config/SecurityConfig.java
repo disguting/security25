27d76be510ffaccd4634bfb46c98018a8d1cad2b
User <thegvazplay@gmail.com>
2025-11-18 21:34:48 +0200

    http secured

--- src/main/java/furman/security25/item/config/SecurityConfig.java
+++ src/main/java/furman/security25/item/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
 
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/archers/**").authenticated()
+                        .requestMatchers("/api/v1/cats/**").authenticated()
                         .anyRequest().permitAll()
                 )
 
